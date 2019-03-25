9ec074fa409b76fae6edf290545cd2f81101b976
Frederic <32013054+Epimodev@users.noreply.github.com>
2019-03-25 14:05:53 +0100

    Increase search debounce duration

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -43,7 +43,7 @@ export class SearchBox extends React.PureComponent<SearchBoxProps, SearchBoxStat
       activeItemIdx: -1,
     };
 
-    this.searchResults = debounce(this.searchResults, 300);
+    this.searchResults = debounce(this.searchResults, 2000);
   }
 
   clear = () => {
